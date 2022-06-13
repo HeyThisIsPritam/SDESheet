1463c06f7d2fc8c40604dfe407359da83ed85655
Pritam Biswas <prit.biswas1996@gmail.com>
2022-06-13 12:08:55 +0530

    Equilibrium push

---
+++ SDESheet/Arrays/Equilibrium.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDESheet.Arrays
+{
+    public class Equilibrium
+    {
+        public Equilibrium(int[] a,int n)
+        {
+            int r_sum = 0;
+            int sum = a.Sum();
+            for (int i = 0; i < n; i++)
+            {
+                sum -= a[i];
+                if (r_sum == sum)
+                    Console.WriteLine(i+1);
+                else
+                    r_sum += a[i];
+            }
+        }
+    }
+}
--- SDESheet/Program.cs
+++ SDESheet/Program.cs
@@ -8,8 +8,12 @@ namespace SDESheet
         static void Main(string[] args)
         {
             //Leaders 
-            int[] arr = new int[] { 16, 17, 4, 3, 5, 2 };
-            Leaders leaders = new Leaders(arr,arr.Length);
+            //int[] arr = new int[] { 16, 17, 4, 3, 5, 2 };
+            //Leaders leaders = new Leaders(arr,arr.Length);
+
+            //Equilibrium
+            int[] arr = { 1, 3, 5, 2, 2 };
+            Equilibrium equilibrium = new Equilibrium(arr, arr.Length);
 
 
             Console.Read();
