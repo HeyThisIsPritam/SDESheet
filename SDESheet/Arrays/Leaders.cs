3aa4a028c7c5c04e89d271102edd568349236134
Pritam Biswas <prit.biswas1996@gmail.com>
2022-06-13 11:17:43 +0530

    Push master

---
+++ SDESheet/Arrays/Leaders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDESheet.Arrays
+{
+    public class Leaders
+    {
+        public Leaders(int[] array,int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int j;
+                for (j = i+1;  j< n; j++)
+                {
+                    if (array[i]<=array[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == n)
+                {
+                    Console.WriteLine(array[i]+" ");
+                }
+            }
+        }
+    }
+}
---
+++ SDESheet/Program.cs
@@ -0,0 +1,18 @@
+﻿using SDESheet.Arrays;
+using System;
+
+namespace SDESheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leaders 
+            int[] arr = new int[] { 16, 17, 4, 3, 5, 2 };
+            Leaders leaders = new Leaders(arr,arr.Length);
+
+
+            Console.Read();
+        }
+    }
+}
